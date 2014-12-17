1ad37bb42148a16413f6ae5c231eb562952011f5
HiteshMatnani <hitesh@cheerz.in>
2014-12-18 00:26:48 +0530

    sample api fire

---
+++ Assets/sensorns.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class sensorns : MonoBehaviour 
+{
+	public void sensortempa()
+	{
+		
+	}
+	public void sensortempb()
+	{
+		
+	}
+	public void glassbreaka()
+	{
+		
+	}
+	public void glassbreakb()
+	{
+		
+	}
+	public void gasleaka()
+	{
+		
+	}
+	public void gasleakb()
+	{
+		
+	}
+	public void motiona()
+	{
+		
+	}
+	public void motionb()
+	{
+		
+	}
+	public void pressurea()
+	{
+		
+	}
+	public void pressureb()
+	{
+		
+	}
+	public void contacta()
+	{
+		
+	}
+	public void contactb()
+	{
+		
+	}
+	public void humiditya()
+	{
+		
+	}
+	public void humidityb()
+	{
+		
+	}
+}
