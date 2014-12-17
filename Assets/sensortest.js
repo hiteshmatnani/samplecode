129160f81f7a8728106251c2f88b81e4de946da2
HiteshMatnani <hitesh@cheerz.in>
2014-12-18 01:00:56 +0530

    sample code with javascrite function

---
+++ Assets/sensortest.js
@@ -0,0 +1,58 @@
+﻿#pragma strict
+
+function sensortempa()
+	{
+		
+	}
+	function sensortempb()
+	{
+		
+	}
+	function glassbreaka()
+	{
+		
+	}
+	function glassbreakb()
+	{
+		
+	}
+	function gasleaka()
+	{
+		
+	}
+	function gasleakb()
+	{
+		
+	}
+	function motiona()
+	{
+		
+	}
+	function motionb()
+	{
+		
+	}
+	function pressurea()
+	{
+		
+	}
+	function pressureb()
+	{
+		
+	}
+	function contacta()
+	{
+		
+	}
+	function contactb()
+	{
+		
+	}
+	function humiditya()
+	{
+		
+	}
+	function humidityb()
+	{
+		
+	}
